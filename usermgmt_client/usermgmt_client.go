9603a52158c9b32443b6a43215d425bce79f7e3e
Renner Poveda <rekefa@gmail.com>
2022-02-13 23:02:38 -0600

    Initial Commit:
    Simple gRPC build in go.

---
+++ usermgmt_client/usermgmt_client.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/rennerp/go-grpc-tutorial/usermgmt"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+
+	}
+
+	defer conn.Close()
+	c := pb.NewUserManagementClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	var new_users = make(map[string]int32)
+	new_users["Allice"] = 43
+	new_users["Bob"] = 19
+
+	for name, age := range new_users {
+		response, error := c.CreateNewUser(ctx, &pb.NewUser{Name: name, Age: age})
+		if error != nil {
+			log.Fatalf("Could not create users %v", error)
+		}
+
+		log.Printf(`User Details:
+NAME: %s
+AGE: %d
+ID: %d`, response.GetName(), response.GetAge(), response.GetId())
+	}
+}
